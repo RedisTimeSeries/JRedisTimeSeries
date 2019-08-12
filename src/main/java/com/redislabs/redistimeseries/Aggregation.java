2b19d0e5924a13eb1e85fa23c032872fcc47df36
Guy Korland <gkorland@gmail.com>
2019-08-12 15:02:25 +0300

    add support for STD.* aggregation commands

--- src/main/java/com/redislabs/redistimeseries/Aggregation.java
+++ src/main/java/com/redislabs/redistimeseries/Aggregation.java
@@ -3,9 +3,14 @@ package com.redislabs.redistimeseries;
 import redis.clients.jedis.util.SafeEncoder;
 
 public enum Aggregation {
-  AVG, SUM, MIN, MAX, RANGE, COUNT, FIRST, LAST;
+  AVG, SUM, MIN, MAX, RANGE, COUNT, FIRST, LAST, 
+  STD_P("STD.P"), STD_S("STD.S"), STD_V("STD.V");  
   
   private final byte[] raw;
+  
+  Aggregation(String alt) {
+    raw = SafeEncoder.encode(alt);
+  }
   
   Aggregation() {
     raw = SafeEncoder.encode(this.name());
