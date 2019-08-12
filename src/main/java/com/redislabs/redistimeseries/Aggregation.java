802dc107b31f98e4aaa2d94e5076a53cbfdbe19b
Guy Korland <gkorland@gmail.com>
2019-08-12 15:10:19 +0300

    fix STD/VAR commands names

--- src/main/java/com/redislabs/redistimeseries/Aggregation.java
+++ src/main/java/com/redislabs/redistimeseries/Aggregation.java
@@ -4,7 +4,7 @@ import redis.clients.jedis.util.SafeEncoder;
 
 public enum Aggregation {
   AVG, SUM, MIN, MAX, RANGE, COUNT, FIRST, LAST, 
-  STD_P("STD.P"), STD_S("STD.S"), STD_V("STD.V");  
+  STD_P("STD.P"), STD_S("STD.S"), VAR_P("VAR.P"), VAR_S("VAR.S");  
   
   private final byte[] raw;
   
